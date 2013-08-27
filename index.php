8ebce6deb240d76ed194be17d8c27319f8589f00
Loïc Goyet <loic.goyet@gmail.com>
2013-08-27 10:56:28 +0200

    installing twig for the project

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
     include_once('twig/lib/Twig/Autoloader.php');
-    require_once 'Faker/src/autoload.php';
     Twig_Autoloader::register();
 
     $loader = new Twig_Loader_Filesystem('templates');
     $twig = new Twig_Environment($loader, array(
       'cache' => false
     ));
-
-    $faker = Faker\Factory::create('fr_FR');
 
     if (is_array($_GET['page'])) {
         $page = implode('/', $_GET['page']);
@@ -17,7 +14,6 @@
     }
 
     echo $twig->render($page . '.html.twig', array(
-        'moteur_name' => 'Twig',
-        'faker' => $faker
+        'moteur_name' => 'Twig'
     ));
 ?>
