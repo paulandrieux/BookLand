db3711440d46d8dd95a0a7d5171dc139c7ee2b9e
Loïc Goyet <loic.goyet@gmail.com>
2013-08-29 10:17:00 +0200

    merge solved

--- php/functions.php
+++ php/functions.php
@@ -9,21 +9,6 @@
 // ---------------------------------------------------------------------------------------------
 function showlastbook($pdo){
 
-<<<<<<< HEAD
- 	
-	$listBook = $pdo->query("SELECT id, creationDate, lastName, firstName, description FROM book ORDER BY creationDate DESC, id LIMIT 7"); // Affichage des 7 derniers books par date de création
-		while ($list=$listBook->fetch())
-			{
-				
-				
-				$initialeLastName = substr($list['lastName'], 0, 1); // Enresigtrement de la première lettre du Nom
-				$initialeFirstName = substr($list['firstName'], 0, 1); // Enresigtrement de la première lettre du Prénom
-				echo $initialeLastName." ".$initialeFirstName.". Sa description : ".$list['description']." et voici l'adresse de son book : 127.0.0.1/bookland/books.php?id=".$list['id'].".<br/>";
-				
-			}
-=======
->>>>>>> 4d1f684d7ae07bfc65ad250cd0056d534586dd1b
-
     $listBook = $pdo->query("SELECT id, creationDate, lastName, firstName, description FROM book ORDER BY creationDate DESC, id LIMIT 7");
         while ($list=$listBook->fetch()) {
             $initialeLastName = substr($list['lastName'], 0, 1);
