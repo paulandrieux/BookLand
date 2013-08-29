4d1f684d7ae07bfc65ad250cd0056d534586dd1b
Loïc Goyet <loic.goyet@gmail.com>
2013-08-29 08:58:54 +0200

    start integration

---
+++ home.php
@@ -0,0 +1,101 @@
+<?php
+    include('./php/pdo.php');
+    include('./php/bookClass.php');
+    include('./php/contactClass.php');
+    include('./php/functions.php');
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>PHP - Affichage par date</title>
+
+        <link href='http://fonts.googleapis.com/css?family=Fauna+One' rel='stylesheet' type='text/css'>
+        <link href="./less/style.css" rel="stylesheet">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    </head>
+
+    <body>
+        <div id="st-container" class="st-container">
+            <div class="st-pusher">
+                <nav class="st-menu st-effect-1" id="menu-1">
+                    <h2 class="icon icon-lab"><img src="img/logoNav.png">Menu</h2>
+                    <ul>
+                        <li><a class="icon active" href="#">Page d'accueil</a></li>
+                        <li><a class="icon" href="#">Page d'inscription</a></li>
+                        <li><a class="icon" href="#">Tout les <span class="book">Book</span></a></li>
+                        <li><a class="icon" href="#">Voir un <span class="book">Book</span></a></li>
+                        <p>Projet réalisé dans le cadre de la formation "développeur web" proposé par le CESI de Nantes. Crée par Julien Latouche, Anthony Martin & Loïc Goyet</p>
+                    </ul>
+                </nav>
+                <div class="st-content"><!-- this is the wrapper for the content -->
+                    <div class="st-content-inner">
+                        <header>
+                            <div class="container">
+                                <a href="home.php" class="title">
+                                    <img src="img/logoBookLand.png">
+                                    BookLand
+                                </a>
+                                <div id="st-trigger-effects" class="column">
+                                    <button data-effect="st-effect-1" class="btn important pull-right"><i class="icon-align-justify"></i> Menu</button>
+                                </div>
+                            </div>
+                        </header>
+                        <div class="headline">
+                            <div class="container">
+                                <h1>- Elevez votre <span class="book">Book</span> au sommet -</h1>
+                                
+                                    
+                                    <a href="#" class="pull-right btn red subscribe"><img src="img/icn/addUser.png">Inscrivez vous maintenant !</a>
+                                
+                            </div>
+                        </div>
+
+                <!-- END COMMUN PART -->
+                        
+                        <div class="content">
+                            <div class="container">
+                                <div class="imgGrid">
+                                    <a href="#" class="imgGrid1 small"></a>
+                                    <a href="#" class="imgGrid2 long"></a>
+                                    <a href="#" class="imgGrid3 large"></a>
+                                    <a href="#" class="imgGrid4 long"></a>
+                                    <a href="#" class="imgGrid5 small"></a>
+                                </div>
+                                <div class="capsule">
+                                    <?php showlastbook($pdo); ?>
+                                </div>
+                            </div>
+                        </div>
+
+                <!-- START COMMUN PART -->
+
+                        <footer></footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Script Library -->
+            <!-- Bootstrap -->
+                <script type="text/javascript" src="js/bootstrap/tooltip.js"></script>
+                <script type="text/javascript" src="js/bootstrap/affix.js"></script>
+                <script type="text/javascript" src="js/bootstrap/alert.js"></script>
+                <script type="text/javascript" src="js/bootstrap/button.js"></script>
+                <script type="text/javascript" src="js/bootstrap/carousel.js"></script>
+                <script type="text/javascript" src="js/bootstrap/collapse.js"></script>
+                <script type="text/javascript" src="js/bootstrap/dropdown.js"></script>
+                <script type="text/javascript" src="js/bootstrap/modal.js"></script>
+                <script type="text/javascript" src="js/bootstrap/popover.js"></script>
+                <script type="text/javascript" src="js/bootstrap/scrollspy.js"></script>
+                <script type="text/javascript" src="js/bootstrap/tab.js"></script>
+                <script type="text/javascript" src="js/bootstrap/transition.js"></script>
+            <!-- Modernizr -->
+                <script type="text/javascript" src="js/modernizr.custom.js"></script>
+                <script src="js/classie.js"></script>
+                <script src="js/sidebarEffects.js"></script>
+            <!-- Custom -->
+                <script type="text/javascript" src="js/bookland.custom.js"></script>
+    </body>
+</html>
--- php/functions.php
+++ php/functions.php
@@ -9,20 +9,15 @@
 
  function showlastbook($pdo){
 
- 	
-	$listBook = $pdo->query("SELECT id, creationDate, lastName, firstName, description FROM book ORDER BY creationDate DESC, id LIMIT 7");
-		while ($list=$listBook->fetch())
-			{
-				
-				
-				$initialeLastName = substr($list['lastName'], 0, 1);
-				$initialeFirstName = substr($list['firstName'], 0, 1);
-				echo $initialeLastName." ".$initialeFirstName.". Sa description : ".$list['description']." et voici l'adresse de son book : 127.0.0.1/bookland/books.php?id=".$list['id'].".<br/>";
-				
-			}
 
-	$listBook->closeCursor();
-	
+    $listBook = $pdo->query("SELECT id, creationDate, lastName, firstName, description FROM book ORDER BY creationDate DESC, id LIMIT 7");
+        while ($list=$listBook->fetch()) {
+            $initialeLastName = substr($list['lastName'], 0, 1);
+            $initialeFirstName = substr($list['firstName'], 0, 1);
+            echo $initialeLastName." ".$initialeFirstName.". Sa description : ".$list['description']." et voici l'adresse de son book : 127.0.0.1/bookland/books.php?id=".$list['id'].".<br/>";
+        }
+
+    $listBook->closeCursor();
 
 }
 ?>
